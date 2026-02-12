1cf1fb2ec1c6bda0ec7fbacb8d64d77409afb508
Rrhar'il·Kitten <77912495+Rrharil-Kittens@users.noreply.github.com>
2026-02-12 08:32:50 +0800

    2

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: 'HikarierKittens_newWebUI',
+  description: 'HikarierKittens_newWebUI',
+})
